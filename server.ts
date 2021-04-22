e2fab36ddbeaf583cb20d48770d12bdb4b11b3cd
India Abbott <indiaabbott@hotmail.com>
2021-04-22 15:44:56 +0100

    updating get names route handler to order by id asc

--- server.ts
+++ server.ts
@@ -41,7 +41,7 @@ app.post("/category", async (req, res) => {
 
 //get all names from database
 app.get("/names", async (req, res) => {
-  const playerName = await client.query('SELECT * FROM players');
+  const playerName = await client.query('SELECT * FROM players ORDER BY id ASC');
   res.json(playerName.rows);
 });
 
